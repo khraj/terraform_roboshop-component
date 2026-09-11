bd85faa8e0409e80ccc76750323b55209bb13fe1
khraj <krishnakoochetti57@gmail.com>
2026-09-11 14:51:21 +0530

    added

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_ami_from_instance" "main" {
   )
 }
 
-resource "aws_lb_target_group" "main" {
+resource "aws_lb_target_group" "this" {
   name     = "${local.common_name_suffix}-${var.component}"
   port     = local.tg_port #if frontend port is 80 for backend it is 8080
   protocol = "HTTP"
