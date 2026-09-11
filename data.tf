1a1fe5816418ba4590b02bf09e61849efd895188
khraj <krishnakoochetti57@gmail.com>
2026-09-11 12:10:54 +0530

    added

--- data.tf
+++ data.tf
@@ -39,3 +39,9 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+}
+
+
+
