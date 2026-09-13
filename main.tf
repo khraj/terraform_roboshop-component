d3421c2ef9381fb99d9343ae8bbb4839809d6979
khraj <krishnakoochetti57@gmail.com>
2026-09-14 00:27:37 +0530

    added

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "terraform_data" "main" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh ${var.component} ${var.environment}"
+        "sudo /tmp/bootstrap.sh ${var.component} ${var.environment}"
     ]
   }
 }
